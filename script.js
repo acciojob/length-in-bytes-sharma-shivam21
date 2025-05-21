fd029a10042bccaedafd4989fd0d290897429583
sharma-shivam21 <s3860762@gmail.com>
2025-05-21 10:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const byteSize = (str) => {
-  // write your code here
+ const byteSize = (str) => {
+  return new Blob([str]).size;
 };
 
+// Test cases
+console.log(byteSize('hello world'));    // 11
+console.log(byteSize('안녕하세요'));        // 15
+console.log(byteSize(''));               // 0
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
